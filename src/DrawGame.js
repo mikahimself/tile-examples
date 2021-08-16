938b9b0e00b6f352f1de92cea70942061e7d9a3b
mikahimself <mika.laihanen@gmail.com>
2021-08-16 19:39:15 +0300

    Add DrawGame class, update map data

---
+++ src/DrawGame.js
@@ -0,0 +1,42 @@
+class DrawGame {
+    constructor(ctx, mapH, mapW, tileH, tileW, mapData) {
+        this.ctx = ctx
+        this.mapData = mapData
+        this.mapH = mapH | 15;
+        this.mapW = mapW | 15;
+        this.tileH = tileH | 32;
+        this.tileW = tileW | 32;
+    }
+
+    drawGrid(mapData) {
+        for (let y = 0; y < this.mapH; y++) {
+            for (let x = 0; x < this.mapW; x++) {
+                switch(mapData[((y * this.mapW) + x)]){
+                    case 0:
+                        this.ctx.fillStyle = "#eeeeee";
+                        break;
+                    case 1:
+                        this.ctx.fillStyle = "#777777";
+                        break;
+                    case 2:
+                        this.ctx.fillStyle = "#999999";
+                        break;
+                }
+                this.ctx.fillRect(x * this.tileW, y * this.tileH, this.tileW, this.tileH);
+            }
+        }
+    }
+
+    drawFramerate(framesLastSecond) {
+        this.ctx.fillStyle = "#000000";
+        this.ctx.fillText(`FPS: ${framesLastSecond}`, 16, 22);
+    }
+
+    drawAll(mapData, framesLastSecond) {
+        if (this.ctx === null) { return; }
+        this.drawGrid(mapData);
+        this.drawFramerate(framesLastSecond);
+    }
+}
+
+export default DrawGame;
--- src/main.js
+++ src/main.js
@@ -1,18 +1,31 @@
-import { MapData } from './mapdata.js'
+import { MapData } from './mapdata.js';
+import DrawGame from './DrawGame.js';
 
 var ctx;
+var drawGame;
 var currentSecond = 0;
 var framesLastSecond = 0;
 var frameCount = 0;
-var mapH = 15;
-var mapW = 15;
-var tileH = 32;
-var tileW = 32;
 
 window.onload = function() {
     ctx = document.getElementById('gameCanvas').getContext('2d');
-    requestAnimationFrame(drawGame);
+    requestAnimationFrame(loop);
+    drawGame = new DrawGame(ctx, 15, 15, 32, 32, MapData);
     ctx.font = 'bold 12pt sans-serif'
+}
+
+function update() {
+    calculateFramerate()
+}
+
+function draw() {
+    drawGame.drawAll(MapData, framesLastSecond)
+}
+
+function loop() {
+    update()
+    draw()
+    window.requestAnimationFrame(loop)
 }
 
 function calculateFramerate() {
@@ -20,39 +33,8 @@ function calculateFramerate() {
     if (sec != currentSecond) {
         currentSecond = sec;
         framesLastSecond = frameCount;
-        frameCount = 0;
+        frameCount = 1;
     } else {
         frameCount++;
     }
-}
-
-function drawGrid() {
-    for (let y = 0; y < mapH; y++) {
-        for (let x = 0; x < mapW; x++) {
-            switch(MapData[((y * mapW) + x)]){
-                case 0:
-                    ctx.fillStyle = "#eeeeee";
-                    break;
-                case 1:
-                    ctx.fillStyle = "#999999";
-                    break;
-            }
-            ctx.fillRect(x * tileW, y * tileH, tileW, tileH);
-        }
-    }
-}
-
-function drawFramerate() {
-    ctx.fillStyle = "#000000";
-    ctx.fillText(`FPS: ${framesLastSecond}`, 16, 22);
-}
-
-function drawGame() {
-    if (ctx === null) { return; }
-
-    calculateFramerate();
-    drawGrid();
-    drawFramerate();
-    
-    window.requestAnimationFrame(drawGame)
 }
